0bceb5c9542f38a4c9ddee3889ddde3f4f4a97ed
Artii-git <64895033+Artii-git@users.noreply.github.com>
2020-05-08 21:00:47 +0530

    Update Java3.java

--- src/Proactive/Java3.java
+++ src/Proactive/Java3.java
@@ -12,6 +12,6 @@ package Proactive;
 public class Java3 {
     public static void main(String args[])
     {
-        System.out.println("Hello Hi");
+        System.out.println("How are you Hello Hi");
     }
 }
